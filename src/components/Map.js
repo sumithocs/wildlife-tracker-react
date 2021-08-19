9626de665d16436e5277a11f0f1b1626f5a836f3
Sumith Samaraweerahetti <sumithocs@gmail.com>
2021-08-19 16:30:25 +1200

    env key value

--- src/components/Map.js
+++ src/components/Map.js
@@ -19,7 +19,7 @@ const Map = ({ eventData, center, zoom }) => {
     return (
         <div className="map">
             <GoogleMapReact
-                bootstrapURLKeys={{ key: `${process.env.REACT_APP_GOOGLE_API_KEY}`}}
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY}}
                 defaultCenter={ center }
                 defaultZoom={ zoom }
             >
